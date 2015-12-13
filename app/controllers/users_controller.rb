60bcea02a6ce58eb9f493cde9c46af8ac17df87d
Brandon Wieber <brandon.m.wieber@gmail.com>
2015-12-13 14:14:45 -0500

    To-Do Items created and displayed after creation on profile page

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,9 +3,11 @@ class UsersController < ApplicationController
   end
 
   def show
-    @user = current_user
-
-    if @user.nil?
+    if !current_user.nil?
+      @user = current_user
+      @item = Item.new
+      @items = @user.items
+    else
       redirect_to new_user_registration_path
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,19 @@
 Rails.application.routes.draw do
 
-  devise_for :users, :controllers => { registrations: 'registrations' }
+  get 'items/create'
 
-  get  'users/show'
+  devise_for :users, :controllers => { registrations: 'registrations' }
 
   get  'welcome/index'
 
   get  'welcome/about'
 
   root 'users#show'
+
+  resources :users do
+    resources :items, only: [:new, :create]
+  end
+
+  resources :items, only: [:new, :create]
 
 end
