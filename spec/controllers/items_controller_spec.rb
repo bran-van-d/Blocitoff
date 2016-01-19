e0971924411c5004ab3032720a2e1d86f3da4770
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-01-18 20:07:07 -0500

    Updated DELETE destroy test on items_controller

--- spec/controllers/items_controller_spec.rb
+++ spec/controllers/items_controller_spec.rb
@@ -1,14 +1,15 @@
 require 'rails_helper'
 
 RSpec.describe ItemsController, type: :controller do
-  let(:my_user) { create(:user) }
-  let(:my_item) { create(:item, user: my_user) }
-
   before(:each) do
     @request.env["devise.mapping"] = Devise.mappings[:user]
-    user = FactoryGirl.create(:user, email: Faker::Internet.email)
-    sign_in user
+    @user = FactoryGirl.create(:user, email: Faker::Internet.email)
+    sign_in @user
   end
+
+  let(:my_item) { create(:item, user: @user) }
+
+
 
   describe "POST create" do
 
@@ -24,14 +25,14 @@ RSpec.describe ItemsController, type: :controller do
 
   describe "DELETE destroy" do
     it "deletes the comment" do
-      delete :destroy, format: :js, user_id: my_user.id, id: my_item.id
+      delete :destroy, format: :js, user_id: @user.id, id: my_item.id
       count = Item.where({id: my_item.id}).count
       expect(count).to eq 0
     end
 
     it "redirects to the user show view" do
-      delete :destroy, format: :js, user_id: my_item.user.id, id: my_item.id
-      expect(respose).to have_http_status(:success)
+      delete :destroy, format: :js, user_id: @user.id, id: my_item.id
+      expect(response).to have_http_status(:success)
     end
   end
 end
