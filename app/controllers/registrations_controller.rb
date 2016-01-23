6de51f6497126214b2dedd17c51257f123a0a027
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-01-23 15:42:48 -0500

    Added registration_controller

---
+++ app/controllers/registrations_controller.rb
@@ -0,0 +1,22 @@
+class RegistrationsController < Devise::RegistrationsController
+
+  private
+
+  def sign_up_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation)
+  end
+
+  def account_update_params
+    params.require(:user).permit(:first_name, :last_name, :email, :password, :password_confirmation, :current_password)
+  end
+
+  protected
+
+  def after_sign_up_path_for(resource)
+    '/users/show'
+  end
+
+  def after_inactive_sign_up_path_for(resource)
+    '/welcome/index'
+  end
+end
