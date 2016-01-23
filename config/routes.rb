672385c990c7c1b76073053c588dde2f7ccdbb0d
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-01-23 12:59:38 -0500

    Adjusted routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users, controllers: { registrations: 'registrations' }
+  devise_for :users
 
   resources :items, only: [:new, :create], path_names: { new: "create" }
   resources :welcome, path: "", only: :index do
