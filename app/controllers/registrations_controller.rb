d4a65ea2178ebb73db60be1471e7db4ea9e56f16
Brandon Wieber <brandon.m.wieber@gmail.com>
2016-01-23 15:45:08 -0500

    Fix

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -13,7 +13,7 @@ class RegistrationsController < Devise::RegistrationsController
   protected
 
   def after_sign_up_path_for(resource)
-    '/users/show'
+    '/welcome/index'
   end
 
   def after_inactive_sign_up_path_for(resource)
